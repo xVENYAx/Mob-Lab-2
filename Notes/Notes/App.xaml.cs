2bc1ad29d9967a3a956b7b7c53e0b36ee811a31c
Sasha <t3t3t3@ukr.net>
2021-05-17 18:39:56 +0300

    Добавив можливість добовляти вкладки.

--- Notes/Notes/App.xaml.cs
+++ Notes/Notes/App.xaml.cs
@@ -1,13 +1,17 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
 
 namespace Notes
 {
     public partial class App : Application
     {
+        public static string FolderPath { get; private set; }
 
         public App()
         {
             InitializeComponent();
+            FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
             MainPage = new AppShell();
         }
 
--- Notes/Notes/Views/NotesPage.xaml.cs
+++ Notes/Notes/Views/NotesPage.xaml.cs
@@ -1,38 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using Notes.Models;
 using Xamarin.Forms;
 
 namespace Notes.Views
 {
     public partial class NotesPage : ContentPage
     {
-        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
-
         public NotesPage()
         {
             InitializeComponent();
-
-            // Read the file.
-            if (File.Exists(_fileName))
-            {
-                editor.Text = File.ReadAllText(_fileName);
-            }
         }
 
-        void OnSaveButtonClicked(object sender, EventArgs e)
+        protected override void OnAppearing()
         {
-            // Save the file.
-            File.WriteAllText(_fileName, editor.Text);
+            base.OnAppearing();
+
+            var notes = new List<Note>();
+
+            // Create a Note object from each file.
+            var files = Directory.EnumerateFiles(App.FolderPath, "*.notes.txt");
+            foreach (var filename in files)
+            {
+                notes.Add(new Note
+                {
+                    Filename = filename,
+                    Text = File.ReadAllText(filename),
+                    Date = File.GetCreationTime(filename)
+                });
+            }
+
+            // Set the data source for the CollectionView to a
+            // sorted collection of notes.
+            collectionView.ItemsSource = notes
+                .OrderBy(d => d.Date)
+                .ToList();
         }
 
-        void OnDeleteButtonClicked(object sender, EventArgs e)
+        async void OnAddClicked(object sender, EventArgs e)
         {
-            // Delete the file.
-            if (File.Exists(_fileName))
+            // Navigate to the NoteEntryPage, without passing any data.
+            await Shell.Current.GoToAsync(nameof(NoteEntryPage));
+        }
+
+        async void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection != null)
             {
-                File.Delete(_fileName);
+                // Navigate to the NoteEntryPage, passing the filename as a query parameter.
+                Note note = (Note)e.CurrentSelection.FirstOrDefault();
+                await Shell.Current.GoToAsync($"{nameof(NoteEntryPage)}?{nameof(NoteEntryPage.ItemId)}={note.Filename}");
             }
-            editor.Text = string.Empty;
         }
     }
 }
